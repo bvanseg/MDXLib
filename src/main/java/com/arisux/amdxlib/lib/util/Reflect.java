dc2503172093a7986289a952acd32bb1a2d36a65
Dustin Christensen <Ri5ux@users.noreply.github.com>
2016-08-03 13:32:14 -0500

    Temporary fix for a reflection problem caused by other mods using access transformers. (Should also use an access transformer)

--- src/main/java/com/arisux/amdxlib/lib/util/Reflect.java
+++ src/main/java/com/arisux/amdxlib/lib/util/Reflect.java
@@ -9,37 +9,100 @@ public class Reflect
 {
     public static double getDouble(Object obj, String deobfName, String obfName)
     {
-        return ((Double) get(obj, deobfName, obfName)).doubleValue();
+        try
+        {
+            return ((Double) get(obj, deobfName, obfName)).doubleValue();
+        }
+        catch (Exception e)
+        {
+            ;
+        }
+
+        return 0D;
     }
 
     public static float getFloat(Object obj, String deobfName, String obfName)
     {
-        return ((Float) get(obj, deobfName, obfName)).floatValue();
+        try
+        {
+            return ((Float) get(obj, deobfName, obfName)).floatValue();
+        }
+        catch (Exception e)
+        {
+            ;
+        }
+
+        return 0F;
     }
 
     public static int getInt(Object obj, String deobfName, String obfName)
     {
-        return ((Integer) get(obj, deobfName, obfName)).intValue();
+        try
+        {
+            return ((Integer) get(obj, deobfName, obfName)).intValue();
+        }
+        catch (Exception e)
+        {
+            ;
+        }
+
+        return 0;
     }
 
     public static boolean getBoolean(Object obj, String deobfName, String obfName)
     {
-        return ((Boolean) get(obj, deobfName, obfName)).booleanValue();
+        try
+        {
+            return ((Boolean) get(obj, deobfName, obfName)).booleanValue();
+        }
+        catch (Exception e)
+        {
+            ;
+        }
+
+        return false;
     }
 
     public static long getLong(Object obj, String deobfName, String obfName)
     {
-        return ((Long) get(obj, deobfName, obfName)).longValue();
+        try
+        {
+            return ((Long) get(obj, deobfName, obfName)).longValue();
+        }
+        catch (Exception e)
+        {
+            ;
+        }
+
+        return 0L;
     }
 
     public static byte getByte(Object obj, String deobfName, String obfName)
     {
-        return ((Byte) get(obj, deobfName, obfName)).byteValue();
+        try
+        {
+            return ((Byte) get(obj, deobfName, obfName)).byteValue();
+        }
+        catch (Exception e)
+        {
+            ;
+        }
+
+        return 0;
     }
 
     public static String getString(Object obj, String deobfName, String obfName)
     {
-        return (String) get(obj, deobfName, obfName);
+        try
+        {
+            return (String) get(obj, deobfName, obfName);
+        }
+        catch (Exception e)
+        {
+            ;
+        }
+
+        return "";
     }
 
     public static void set(Object obj, String deobfName, String obfName, Object value)
