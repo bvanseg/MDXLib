a32cb751b56959c10034209bd70b6667ea166219
Dustin Christensen <Ri5ux@users.noreply.github.com>
2017-01-10 02:19:14 -0600

    Build script was not injecting the build number into the sources.

--- src/main/java/com/arisux/mdxlib/MDX.java
+++ src/main/java/com/arisux/mdxlib/MDX.java
@@ -29,7 +29,7 @@ import net.minecraft.nbt.CompressedStreamTools;
 public class MDX
 {
     public static final String                          MODID   = "mdxlib";
-    public static final String                          VERSION = "1.0.0";
+    public static final String                          VERSION = "@VERSION@";
 
     /** Provides access to core Minecraft methods that have restricted access **/
     private static Access                               access;
