989f55dde77a83fd0864cc56cd35a369286d5986
Dustin Christensen <Ri5ux@users.noreply.github.com>
2017-12-15 22:14:36 -0600

    Improved the cloud movement mechanism.

--- src/main/java/com/arisux/mdx/lib/client/render/world/ICloudProvider.java
+++ src/main/java/com/arisux/mdx/lib/client/render/world/ICloudProvider.java
@@ -1,6 +1,8 @@
 package com.arisux.mdx.lib.client.render.world;
 
 import com.arisux.mdx.lib.client.render.Texture;
+import com.arisux.mdx.lib.game.Game;
+import com.arisux.mdx.lib.util.MDXMath;
 
 import net.minecraft.world.World;
 
@@ -10,5 +12,13 @@ public interface ICloudProvider
     
     public Texture getCloudTexture();
     
-    public float getCloudMovementDirection(World world);
+    public default double getCloudMovementX(World world, float cloudTicksPrev, float cloudTicks)
+    {
+        return MDXMath.interpolateRotation(cloudTicksPrev, cloudTicks, Game.partialTicks());
+    }
+    
+    public default double getCloudMovementZ(World world, float cloudTicksPrev, float cloudTicks)
+    {
+        return 0;
+    }
 }
