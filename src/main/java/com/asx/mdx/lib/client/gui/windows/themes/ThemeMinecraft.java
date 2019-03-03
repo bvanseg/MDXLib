d0b39e26ac54197689aebf60455e0ba5530d0065
Dustin Christensen <Ri5ux@users.noreply.github.com>
2019-03-03 03:11:10 -0600

    Fixed the built in Window Manager example theme. (The Minecraft Theme)

--- src/main/java/com/asx/mdx/lib/client/gui/windows/themes/ThemeMinecraft.java
+++ src/main/java/com/asx/mdx/lib/client/gui/windows/themes/ThemeMinecraft.java
@@ -1,6 +1,9 @@
 package com.asx.mdx.lib.client.gui.windows.themes;
 
 import com.asx.mdx.lib.client.gui.windows.Window;
+import com.asx.mdx.lib.client.util.Draw;
+
+import net.minecraft.init.Blocks;
 
 public class ThemeMinecraft extends Theme implements ITheme
 {
@@ -17,16 +20,16 @@ public class ThemeMinecraft extends Theme implements ITheme
 
     public void drawBackground(Window window, int mouseX, int mouseY)
     {
-        // RenderLib.drawBlockSide(43, 2, window.getX(), window.getY() - 16, window.getWidth(), 16, window.getWidth() / 50.0F, 0.5F);
+        Draw.drawBlock(Blocks.SANDSTONE.getDefaultState(), window.getX(), window.getY() - 16, window.getWidth(), 16, window.getWidth() / 16F, 1F);
     }
 
     public void drawTitleBar(Window window, int mouseX, int mouseY)
     {
-        // RenderLib.drawBlockSide(1, 0, window.getX(), window.getY(), window.getWidth(), window.getHeight(), window.getWidth() / 50.0F, window.getHeight() / 50.0F);
+        Draw.drawBlock(Blocks.SAND.getDefaultState(), window.getX(), window.getY(), window.getWidth(), window.getHeight(), window.getWidth() / 16F, window.getHeight() / 16F);
     }
 
     public void drawCloseButton(Window window, int mouseX, int mouseY)
     {
-        // RenderLib.drawBlockSide(46, 2, window.getX() + window.getWidth() - 14, window.getY() - 14, 12, 12);
+        Draw.drawBlock(Blocks.TNT.getDefaultState(), window.getX() + window.getWidth() - 16, window.getY() - 16, 16, 16);
     }
 }
