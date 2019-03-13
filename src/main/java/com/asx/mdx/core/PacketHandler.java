07b67a53bbf894fc99862c43aa580e2f269a9f34
Dustin Christensen <Ri5ux@users.noreply.github.com>
2019-03-12 21:55:09 -0500

    Registered the pause animation packet.

--- src/main/java/com/asx/mdx/core/PacketHandler.java
+++ src/main/java/com/asx/mdx/core/PacketHandler.java
@@ -3,6 +3,7 @@ package com.asx.mdx.core;
 import com.asx.mdx.MDX;
 import com.asx.mdx.core.mods.IInitEvent;
 import com.asx.mdx.core.network.server.PacketAnimation;
+import com.asx.mdx.core.network.server.PacketAnimationPause;
 import com.asx.mdx.core.network.server.PacketCommandBlockScanner;
 import com.asx.mdx.core.network.server.PacketCommandChunkBorders;
 
@@ -28,6 +29,7 @@ public class PacketHandler extends SimpleNetworkWrapper implements IInitEvent
         this.registerMessage(Side.CLIENT, PacketCommandBlockScanner.class);
         this.registerMessage(Side.CLIENT, PacketCommandChunkBorders.class);
         this.registerMessage(Side.CLIENT, PacketAnimation.class);
+        this.registerMessage(Side.CLIENT, PacketAnimationPause.class);
     }
 
     /**
